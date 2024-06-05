5000e4e0783a0515f88204ac11329460690defb5
Emad Iddin Ali <ea224pn@student.lnu.se>
2024-06-05 19:09:03 +0200

    Add project files.

---
+++ array uppgift 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Inlamning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meningar = { "Första meningen.", "Andra meningen.", "Tredje meningen." };
+            foreach (string mening in meningar)
+            {
+                Console.WriteLine(mening + "\n");
+            }
+        }
+    }
+}
